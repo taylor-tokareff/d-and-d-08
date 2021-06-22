4394f02d5e95756329b0fb5923589e4acfa2d551
taylor-tokareff <taylortokareff@gmail.com>
2021-06-21 21:08:15 -0700

    post route failing as expected

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,24 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  test('creates a npc via POST', async () => {
+
+    const res = await request(app)
+      .post('/api/v1/npcs')
+      .send({
+        name: 'mable',
+        age: 69,
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'mable',
+      age: 69,
+    });
+
+  });
+
+
+
 });
